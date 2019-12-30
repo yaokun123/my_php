554c594978a3f3c2cc42511543963567dde1dc1d
yaokun <yaok@pingansec.com>
2019-12-30 19:15:27 +0800

    多进程pstree

--- 系统编程/02多进程/02多进程pstree.php
+++ 系统编程/02多进程/02多进程pstree.php
@@ -28,6 +28,18 @@ while (true){
 /**
  * 上面的代码子进程和父进程都是执行相同的代码，有没有办法让子进程和父进程做不同的事呢，
  * 最简单的办法就是if判断，子进程执行子进程的代码，父进程执行父进程的代码
+ *
+ *
+ * |-+= 00409 yaok /Applications/iTerm.app/Contents/MacOS/iTerm2 -psn_0_86037
+ * | |-+= 72423 yaok /Applications/iTerm.app/Contents/MacOS/iTerm2 --server login -fp yaok
+ * | | \-+= 72424 root login -fp yaok
+ * | |   \-+= 72425 yaok -bash
+ * | |     \-+= 15297 yaok php 02多进程pstree.php【主进程】
+ * | |       |--- 15298 yaok php 02多进程pstree.php【子进程】
+ * | |       |--- 15299 yaok php 02多进程pstree.php【子进程】
+ * | |       |--- 15300 yaok php 02多进程pstree.php【子进程】
+ * | |       |--- 15301 yaok php 02多进程pstree.php【子进程】
+ * | |       \--- 15302 yaok php 02多进程pstree.php【子进程】
  */
 
 //见下一节
