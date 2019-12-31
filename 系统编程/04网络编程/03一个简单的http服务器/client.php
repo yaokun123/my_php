228dc14e37ac405a87a0afe3dd5b81fda723e338
yaokun <yaok@pingansec.com>
2019-12-31 10:49:19 +0800

    一个简单的http服务器

---
+++ 系统编程/04网络编程/03一个简单的http服务器/client.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yaok
+ * Date: 2019/12/31
+ * Time: 上午10:20
+ */
+
+$client_socket = socket_create(AF_INET,SOCK_STREAM,SOL_TCP);
+
+socket_connect($client_socket,'127.0.0.1',8888);
+
+while (1){
+    fwrite(STDOUT,'>>');
+
+    $content = fgets(STDIN);
+
+    socket_write($client_socket,$content);
+
+    $data = socket_read($client_socket,1024);
+
+    if(!$data){
+        break;
+    }
+    echo '服务端返回数据'.$data.PHP_EOL;
+}
