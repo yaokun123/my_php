1ef9948247cac906530f236ef66bd2e7611f30d7
yaokun <yaok@pingansec.com>
2020-04-15 10:25:15 +0800

    工厂模式

--- 设计模式/3简单工厂模式.php
+++ 设计模式/3简单工厂模式.php
@@ -61,3 +61,28 @@ $sqlite->conn();
 
 // 在面向对象设计法则中，重要的开闭原则---对于修改是封闭的，对于扩展是开放的
 // 改进见4工厂模式.php
+
+
+
+echo '==================================='.PHP_EOL;
+//其实工厂模式可以这么改进
+class Factory2{
+    public static function createDB($type){
+
+        //实际中类名跟$type会有稍微的差别，
+        //比如$type='mysql'，类='Mysql'，这时候就要讲$type首字母转换为大写，再去判断类是否存在，
+        //只要这个规则定义好就行
+        //这样的话扩展就不需要再修改Factory2了，直接新增对应数据库类
+        if(class_exists($type)){
+            return new $type();
+        }else{
+            throw new Exception("Error db type",1);
+        }
+    }
+}
+$mysql = Factory::createDB('mysql');
+$mysql->conn();
+
+
+$sqlite = Factory::createDB('sqlite');
+$sqlite->conn();
